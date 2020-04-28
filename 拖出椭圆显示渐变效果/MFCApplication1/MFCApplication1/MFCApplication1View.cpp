b3e3c00ea361fef046821d49d1d609c7b2d4678d
moonstar-MLF <674237772@qq.com>
2020-04-28 20:54:16 +0800

    20200420实验

---
+++ 拖出椭圆显示渐变效果/MFCApplication1/MFCApplication1/MFCApplication1View.cpp
@@ -0,0 +1,120 @@
+
+// MFCApplication1View.cpp : CMFCApplication1View 类的实现
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "MFCApplication1.h"
+#endif
+
+#include "MFCApplication1Doc.h"
+#include "MFCApplication1View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMFCApplication1View
+
+IMPLEMENT_DYNCREATE(CMFCApplication1View, CView)
+
+BEGIN_MESSAGE_MAP(CMFCApplication1View, CView)
+	ON_WM_LBUTTONDOWN()
+	ON_WM_LBUTTONUP()
+	ON_WM_MOUSEMOVE()
+END_MESSAGE_MAP()
+
+// CMFCApplication1View 构造/析构
+
+CMFCApplication1View::CMFCApplication1View()
+{
+	// TODO: 在此处添加构造代码
+	flag = 0;
+	originpoint= NULL;
+	finalpoint = NULL;
+}
+
+CMFCApplication1View::~CMFCApplication1View()
+{
+}
+
+BOOL CMFCApplication1View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CMFCApplication1View 绘制
+
+void CMFCApplication1View::OnDraw(CDC* pDC)
+{
+	CMFCApplication1Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+	if (flag == 1)
+	{
+	CClientDC dc(this);
+	CRect rc;
+	GetClientRect(&rc);
+	pDC->Ellipse(originpoint.x, originpoint.y, finalpoint.x, finalpoint.y);
+	
+	}
+}
+
+
+// CMFCApplication1View 诊断
+
+#ifdef _DEBUG
+void CMFCApplication1View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CMFCApplication1View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CMFCApplication1Doc* CMFCApplication1View::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMFCApplication1Doc)));
+	return (CMFCApplication1Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMFCApplication1View 消息处理程序
+
+
+void CMFCApplication1View::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	flag = 1;
+	originpoint = point;
+	CView::OnLButtonDown(nFlags, point);
+}
+
+
+void CMFCApplication1View::OnLButtonUp(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	flag = 0;
+	CView::OnLButtonUp(nFlags, point);
+}
+
+
+void CMFCApplication1View::OnMouseMove(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	finalpoint = point;
+	Invalidate(true);
+	CView::OnMouseMove(nFlags, point);
+}
